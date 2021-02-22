5fd3cdae08335751210b8c22cecc243923177d2f
Adam Stankiewicz <as1993@o2.pl>
2021-02-22 22:36:38 +0100

    repaired frontend v.2

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-   const apiRoot = 'https://hidden-brushlands-36057.herokuapp.com/v1/task/';
-   const trelloApiRoot = 'https://hidden-brushlands-36057.herokuapp.com/v1/trello/';
+   const apiRoot = 'https://blooming-woodland-26834.herokuapp.com/v1/task/';
+   const trelloApiRoot = 'https://blooming-woodland-26834.herokuapp.com/v1/trello/';
    const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
    const $tasksContainer = $('[data-tasks-container]');
 
